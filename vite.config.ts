a41ae8114defcce72d3cb6bfc3fc0729006eb61c
Hamid <hamid@hamsaa.ir>
2024-12-22 11:59:41 +0330

    feat: adds project path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@context', replacement: path.resolve(__dirname, 'src/context') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@storage', replacement: path.resolve(__dirname, 'src/storage') },
+      { find: '@router', replacement: path.resolve(__dirname, 'src/router') },
+    ],
+  },
 })
