cc91507a7bd4583af34e52313329b4156ea0fb23
Hamid <hamid@hamsaa.ir>
2024-12-23 08:52:18 +0330

    feat: login page with login form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,21 @@
-
+import {useRef} from "react";
+import {LoginFormRef} from "@pages/login/index.type.ts";
+import LoginForm from "@components/form/login";
 
 
 const Login = ()=>{
 
+    const formRef = useRef<LoginFormRef>(null);
+
+    const handleLoginSuccess = (token: string) => {
+        alert(`Login successful! Token: ${token}`);
+    };
 
     return (
-        <div>
-            login
+        <div className="w-full h-screen bg-gray-100 flex items-center justify-center">
+            <LoginForm ref={formRef} onLoginSuccess={handleLoginSuccess}/>
         </div>
-    )
+    );
 }
 
 
