252df7af131a98a322135aabf5861abb2117afee
Hamid <hamid@hamsaa.ir>
2024-12-23 08:53:36 +0330

    fix: removes comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css'; // Import Tailwind CSS
 import AppRouter from "@router/index.tsx";
 
 createRoot(document.getElementById('root')!).render(
