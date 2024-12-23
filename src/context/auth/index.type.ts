b2a36ffb2a167a61e37a3e0949be67fda7250ecb
Hamid <hamid@hamsaa.ir>
2024-12-23 09:51:48 +0330

    feat: adds user to AuthContextType

--- src/context/auth/index.type.ts
+++ src/context/auth/index.type.ts
@@ -3,5 +3,6 @@
 export interface AuthContextType {
     token: string | null;
     login: (token: string) => void;
+    user: { id: number; avatar: string; name: string; email: string } | null;
     logout: () => void;
 }
