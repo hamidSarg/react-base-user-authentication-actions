9991519afefac5d2ef5419c8b4d4a3a4ea457aba
Hamid <hamid@hamsaa.ir>
2024-12-23 08:52:58 +0330

    feat: adds NetworkServiceProvider to app router as a provider wrapper

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import {AuthProvider} from "@context/index.tsx";
+import {AuthProvider} from "@context/auth";
 import ProtectedRoute from "@router/protected";
 import Login from "@pages/login/index.tsx";
 import Dashboard from "@pages/dashboard/index.tsx";
+import {NetworkServiceProvider} from "@context/http";
 
 
 const AppRouter: React.FC = () => {
     return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<ProtectedRoute />}>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                    </Route>
-                    <Route path="*" element={<Navigate to="/login" replace />} />
-                </Routes>
-            </Router>
-        </AuthProvider>
+        <NetworkServiceProvider>
+            <AuthProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/" element={<ProtectedRoute />}>
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Route>
+                        <Route path="*" element={<Navigate to="/login" replace />} />
+                    </Routes>
+                </Router>
+            </AuthProvider>
+        </NetworkServiceProvider>
     );
 };
 
