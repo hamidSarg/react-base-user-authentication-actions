1229330d35a6dc060b06bcfdcbcaefa2d288705e
Hamid <hamid@hamsaa.ir>
2024-12-23 09:51:07 +0330

    feat: fetches CurrentUser and saves it to conext

--- src/context/auth/index.tsx
+++ src/context/auth/index.tsx
@@ -1,11 +1,33 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, {createContext, useContext, useEffect, useState} from 'react';
 import LocalStorageService from "../../storage";
 import {AuthContextType} from "./index.type.ts";
+import {useHttpService} from "@context/http";
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [token, setToken] = useState<string | null>(LocalStorageService.get<string>('token'));
+    const [user, setUser] = useState<AuthContextType["user"]>(null);
+    const httpService = useHttpService();
+
+    const fetchCurrentUser = async () => {
+        if (!token) return;
+
+        try {
+            const response = await httpService.get<{ data: { id: number; avatar: string; first_name: string; last_name: string; email: string } }>(
+                "/users/2" // Replace with the actual endpoint for fetching current user
+            );
+            const userData = response.data;
+            setUser({
+                id: userData.id,
+                avatar: userData.avatar,
+                name: `${userData.first_name} ${userData.last_name}`,
+                email: userData.email,
+            });
+        } catch (error) {
+            console.error("Error fetching current user details:", error);
+        }
+    };
 
     const login = (newToken: string) => {
         setToken(newToken);
@@ -14,11 +36,18 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
     const logout = () => {
         setToken(null);
-        LocalStorageService.remove('token');
+        setUser(null);
+        LocalStorageService.remove("token");
     };
 
+    useEffect(() => {
+        if (token) {
+            fetchCurrentUser();
+        }
+    }, [token]);
+
     return (
-        <AuthContext.Provider value={{ token, login, logout }}>
+        <AuthContext.Provider value={{ token, login, logout,user }}>
             {children}
         </AuthContext.Provider>
     );
