2a466def08b09e6e8e5193357df5feb20aee0d2f
Hamid <hamid@hamsaa.ir>
2024-12-23 09:12:42 +0330

    feat: navigates to dashboard when token is provided

--- src/router/protected/index.tsx
+++ src/router/protected/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {Navigate, Outlet, useNavigate} from 'react-router-dom';
 import {useAuth} from "@context/auth";
 
 const ProtectedRoute: React.FC = () => {
     const { token } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (token) {
+            navigate("/dashboard");
+        }
+    }, [token]);
 
     return token ? <Outlet /> : <Navigate to="/login" replace />;
 };
